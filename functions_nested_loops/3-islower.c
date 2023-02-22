61ba2d5033465d18f40667e2acebb40cfc4d3cd5
Mohamed Djamaldine <djamaldine-mohamed@live.fr>
2023-02-22 06:16:06 -0800

    task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - checks for lowcase charac
+ * @c : the charac to be check
+ * Return: 1 for lowcase chara or 0 for other
+ */
+
+int _islower(int c)
+{
+if (c >= 'a'  && c <= 'z')
+
+return (1);
+ 
+else
+   
+return (0);
+}
