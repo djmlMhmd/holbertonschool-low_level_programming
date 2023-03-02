aef689804ede2696cb08eeb6bf93a86c5da02253
Mohamed Djamaldine <djamaldine-mohamed@live.fr>
2023-03-02 13:36:23 -0800

    task 0 - 2

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: pointer to the destination memory area
+ * @src: pointer to the source memory area
+ * @n: the number of bytes to be copied
+ *
+ * Return: pointer to dest
+ */
+
+#include "main.h"
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+
+return (dest);
+}
