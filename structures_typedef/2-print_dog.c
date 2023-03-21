989e50915135d97e5e76727d7d9a859d2ed922f1
Mohamed Djamaldine <djamaldine-mohamed@live.fr>
2023-03-21 01:17:28 -0700

    Write a function that prints a struct dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,8 +12,8 @@ void print_dog(struct dog *d)
 {
 if (d == NULL)
 return;
-printf("name: %s\n", d->name ? d->name : "(nil)");
-printf("age: %.6f\n", d->age);
-printf("owner: %s\n", d->owner ? d->owner : "(nil)");
+printf("Name: %s\n", d->name ? d->name : "(nil)");
+printf("Age: %.6f\n", d->age);
+printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 
 }
