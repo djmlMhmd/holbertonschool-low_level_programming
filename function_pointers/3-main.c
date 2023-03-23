badfe09bf935199876eeed485c0228dd9277fadf
Mohamed Djamaldine <djamaldine-mohamed@live.fr>
2023-03-23 08:42:19 -0700

    program that performs simple operations.

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -14,29 +14,28 @@ int main(int argc, char *argv[])
 {
 int a, b, result;
 
-	int (*op_func)(int, int);
+int (*op_func)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	op_func = get_op_func(argv[2]);
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+op_func = get_op_func(argv[2]);
 
 
-	if (!op_func)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	if ((op_func == op_div || op_func == op_mod) && b == 0)
-	{
-	  printf("Error\n");
-	  exit(100);
+if (!op_func)
+{
+printf("Error\n");
+exit(99);
+}
+if ((op_func == op_div || op_func == op_mod) && b == 0)
+{
+printf("Error\n");
+exit(100);
 }
 result = op_func(a, b);
 printf("%d\n", result);
