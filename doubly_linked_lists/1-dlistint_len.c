8c8ec46264d186b29435db6010e7f5cf1b11cd9b
djamal <djamaldine-mohamed@live.fr>
2023-04-06 15:52:48 -0700

    print list & add node at the end

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,20 @@
 #include "lists.h"
-#include <stddef.h>
+
 /**
- * print_dlistint - Print all elements of a dlistint_t list
+ * dlistint_len - Return the number of nodes in a dlistint_t list
  * @h: Pointer to the head of the dlistint_t list
  *
  * Return: Number of nodes in the list
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *current = h;
 	size_t count = 0;
 
 	while (current != NULL)
 	{
-		printf("%d\n", current->n);
-		current = current->next;
 		count++;
+		current = current->next;
 	}
 
 	return (count);
