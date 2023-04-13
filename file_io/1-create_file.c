d9bf6ad609838548b1978ba4f81ad06f9c72b778
djamal <djamaldine-mohamed@live.fr>
2023-04-12 22:19:20 -0700

    fixe all betty error

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,30 +12,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, bytes_written, length = 0;
+int fd, bytes_written, length = 0;
 
-    if (filename == NULL)
-        return (-1);
+if (filename == NULL)
+return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[length])
-            length++;
-    }
+if (text_content != NULL)
+{
+while (text_content[length])
+length++;
+}
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
-    if (fd == -1)
-        return (-1);
+if (fd == -1)
+return (-1);
 
-    bytes_written = write(fd, text_content, length);
+bytes_written = write(fd, text_content, length);
 
-    if (bytes_written == -1)
-    {
-        close(fd);
-        return (-1);
-    }
+if (bytes_written == -1)
+{
+close(fd);
+return (-1);
+}
 
-    close(fd);
-    return (1);
+close(fd);
+return (1);
 }
